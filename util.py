addb808b24347ebfb6ff3549e3f1198fe7615a94
Chode-Nithin <135514529+Chode-Nithin@users.noreply.github.com>
2023-06-26 12:18:15 +0530

    Update util.py

--- util.py
+++ util.py
@@ -4,41 +4,45 @@ from PIL import Image
 import numpy as np
 import streamlit as st
 
-
 labels = ['real', 'fake']
 st.title("Authenticity_Detection")
-
 st.write("Upload the picture here!")
 
-# def load_model():
-#     global model
-#     model = tf.keras.models.load_model('fakevsreal_weights.h5')
-@st.cache_resource()
+@st.cache
 def get_model():
     model_path ='fakevsreal_weights.h5'
     model = tf.keras.models.load_model(model_path)
     return model
+
 file_uploaded = st.file_uploader("Choose the Image File", type=["jpg", "png", "jpeg"])
 
 def classify_image(file_uploaded):
-    model=get_model()
+    if file_uploaded is not None:
+        model = get_model()
 
-    image = Image.open(file_uploaded) # reading the image
-    image = image.resize((128, 128)) # resizing the image to fit the trained model
-    image = image.convert("RGB") # converting the image to RGB
-    img = np.asarray(image) # converting it to numpy array
-    img = np.expand_dims(img, 0)
-    predictions = model.predict(img) # predicting the label
-    label = labels[np.argmax(predictions[0])] # extracting the label with maximum probablity
-    probab = float(round(predictions[0][np.argmax(predictions[0])]*100, 2))
+        image = Image.open(file_uploaded) # reading the image
+        image = image.resize((128, 128)) # resizing the image to fit the trained model
+        image = image.convert("RGB") # converting the image to RGB
+        img = np.asarray(image) # converting it to numpy array
+        img = np.expand_dims(img, 0)
+        predictions = model.predict(img) # predicting the label
+        label = labels[np.argmax(predictions[0])] # extracting the label with maximum probability
+        probab = float(round(predictions[0][np.argmax(predictions[0])]*100, 2))
 
-    result = {
-        'label': label,
-        'probablity': probab
-    }
+        result = {
+            'label': label,
+            'probability': probab
+        }
 
-    return result
-rs =classify_image(file_uploaded)
-st.write("Label:", rs['label'])
-st.write("Probability:", rs['probability'])
+        return result
+    else:
+        return None
 
+rs = classify_image(file_uploaded)
+
+if rs is not None:
+    st.write("Label:", rs['label'])
+    st.write("Probability:", rs['probability'])
+
+
+    
