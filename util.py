e5dfe65062e99aa0d5513617fcbf817764b163f1
Chode-Nithin <135514529+Chode-Nithin@users.noreply.github.com>
2023-06-26 18:25:05 +0530

    Update util.py

--- util.py
+++ util.py
@@ -2,16 +2,18 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 import streamlit as st
-from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 
 labels = ['real', 'fake']
+probab = 0
+
 st.title("Authenticity_Detection")
 st.write("Upload the picture here!")
 
 @st.cache_resource()
 def get_model():
-    model = ResNet50(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
+    model_path = 'fakevsreal_weights.h5'
+    model = tf.keras.models.load_model(model_path)
     return model
 
 file_uploaded = st.file_uploader("Choose the Image File", type=["jpg", "png", "jpeg"])
@@ -21,16 +23,30 @@ def classify_image(file_uploaded):
         model = get_model()
 
         image = Image.open(file_uploaded)  # reading the image
-        image = image.resize((128, 128))  # resizing the image to fit the model input shape
+        image = image.resize((128, 128))  # resizing the image to fit the trained model
         image = image.convert("RGB")  # converting the image to RGB
         img = np.asarray(image)  # converting it to numpy array
-        img = preprocess_input(img)  # preprocess the image
         img = np.expand_dims(img, 0)
-        predictions = model.predict(img)
-        label = labels[np.argmax(predictions[0])] # extracting the label with maximum probability
-        probab = float(predictions[0][np.argmax(predictions[0])])# extracting image features using ResNet50
+        
+        # Preprocess the image using ResNet50 preprocessing
+        img = preprocess_input(img)
 
-        # Add your classification code here based on the extracted features
+        # Create an instance of the ResNet50 model
+        resnet_model = ResNet50(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
+        resnet_model.trainable = False
+
+        # Pass the input through the ResNet50 model
+        resnet_output = resnet_model(img)
+
+        # Flatten the output tensor
+        flattened_output = tf.keras.layers.Flatten()(resnet_output)
+
+        # Pass the flattened output through your existing model's layers
+        predictions = model(flattened_output)
+
+        # Extract the label with maximum probability
+        label = labels[np.argmax(predictions[0])]
+        probab = float(predictions[0][np.argmax(predictions[0])])
 
         result = {
             'label': label,
@@ -44,8 +60,11 @@ def classify_image(file_uploaded):
 rs = classify_image(file_uploaded)
 
 if rs is not None:
-    st.write("Label:", rs['label'])
+    st.write("Your Image is:", rs['label'])
     st.write("Probability:", rs['probability'])
+
+
+
 
 # import tensorflow as tf
 # from PIL import Image
